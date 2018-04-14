59514dd51cd2d7782fa2b89113fe0c2403c7bffc
Reno Sorensen <renojames19@gmail.com>
2018-04-14 22:34:29 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -4,7 +4,13 @@ class School
 
   def initialize(name)
     @school = name
+<<<<<<< HEAD
     @roster = {}
+=======
+    def roster
+      @roster = {}
+    end
+>>>>>>> 7daa4397c18b46a2b91b0efa01492fb8f2efe05b
   end
 
   def add_student(name, grade)
@@ -12,7 +18,10 @@ class School
     @roster[grade] << name
   end
 
+<<<<<<< HEAD
   attr_reader :roster
 
 
+=======
+>>>>>>> 7daa4397c18b46a2b91b0efa01492fb8f2efe05b
 end
